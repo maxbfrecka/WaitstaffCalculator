4a37e1e13fc2ba8dbe59f26f6809b864609d74e4
maxbfrecka <maxbfrecka@gmail.com>
2016-11-12 22:38:19 -0500

    with services

--- controller.js
+++ controller.js
@@ -1,38 +1,110 @@
-angular.module('myApp', ['ngMessages'])
-    .controller('WaitstaffController', function() {
-    	var _this = this;
-        this.basePrice=0;
-        this.taxRate=0;
-        this.tipPercentage=0;
+angular.module('myApp', ['ngRoute'])
 
-        this.mealCount=0;
-        this.tipTotal=0;
-        this.basePrice=0;
-        this.averageTip=0;
-        this.subtotal=0;
-        this.tip=0;
-        this.tipTotal=0;
-        this.averageTip=0;
-        this.total=0;
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl',
+            controllerAs : 'ws'
+        })
+        .when('/home', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl',
+            controllerAs : 'ws'
+        })
+        .when('/newmeal', {
+            templateUrl : 'newmeal.html',
+            controller : 'MealCtrl',
+            controllerAs: 'ws'
+        })
+        .when('/myearnings', {
+            templateUrl : 'myearnings.html',
+            controller : 'EarningsCtrl',
+            controllerAs: 'ws'
+        })
+        .when('/error', {
+            template : '<p>Error - Page Not Found</p>'
+        })
+        .otherwise('/error');
+    }])
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+            $rootScope.$on('$routeChangeError', function() {
+                $location.path('/error');
+            });
+        }])
+    .factory('meals', function() {
+        var savedData = {
+            //first settings
+            basePrice: null,
+            taxRate: null,
+            tipRate: null,
 
-        this.submit = function(){
-            _this.tax_rate= _this.taxRate/100;
-            _this.tip_rate= _this.tipPercentage/100;
-            _this.subtotal= _this.basePrice*_this.tax_rate;
-            _this.tip = _this.basePrice*_this.tip_rate;
-            _this.total = _this.basePrice+_this.subtotal+_this.tip;
-            _this.mealCount+=1;
-            _this.tipTotal+=_this.tip;
-            _this.averageTip=(_this.tipTotal+_this.tip)/_this.mealCount;
+            //customer charges
+            subtotal: 0,
+            tip: 0,
+            total:0,
+            
+            //running total
+            tipTotal: 0,
+            mealCount: 0,
+            averageTip: 0
+
         };
 
-        this.cancel = function(){
-            _this.basePrice='';
-            _this.taxRate='';
-            _this.tipPercentage='';
+        savedData.set = function(data){
+            return savedData;
+        }
+        savedData.calculate = function(){
+            savedData.subtotal=savedData.basePrice+(savedData.basePrice*(savedData.taxRate/100));
+            savedData.tip=savedData.basePrice*(savedData.tipRate/100);
+            savedData.total=savedData.tip+savedData.subtotal;
+            savedData.tipTotal+=savedData.tip;
+            savedData.mealCount+=1;
+            savedData.averageTip=(savedData.tipTotal+savedData.tip)/savedData.mealCount;
+            savedData.basePrice=null;
+            savedData.taxRate=null;
+            savedData.tipRate=null;
+            return savedData;
+        }
+
+        savedData.reset = function(){
+            savedData.basePrice= null;
+            savedData.taxRate= null;
+            savedData.tipRate= null;
+            savedData.subtotal= 0;
+            savedData.tip= 0;
+            savedData.total=0;
+            savedData.tipTotal= 0;
+            savedData.mealCount= 0;
+            savedData.averageTip= 0;
+            return savedData;
+        }
+
+        return savedData;
+       
+    })
+    .controller('HomeCtrl', function(meals) {
+    })
+    .controller('MealCtrl', function(meals) {
+        var ws = this;
+        ws.meal = meals;
+
+        ws.submit = function(){
+            ws.meal.calculate();
         };
 
-        this.double = function(value){return value*2;};
+        ws.cancel = function(){
+            ws.meal.basePrice='';
+            ws.meal.taxRate='';
+            ws.meal.tipRate='';
+        };
 
+    }).controller('EarningsCtrl', function(meals) {
+        var ws = this;
+        ws.meal = meals;
 
+        ws.reset = function(){
+            ws.meal.reset();
+        }
     });
+
+
